469c69c672e5651fb43bd00b192f8b8686897a57
Fuad Muhammed <fuad2dev@gmail.com>
2023-07-10 02:33:58 +0000

    feat: stat bar

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,6 +7,7 @@ import { ref } from "vue";
 import SectionGroup from "@/Components/SectionGroup.vue";
 
 const toggleState = ref(0);
+const showStatBar = ref(true);
 let increaseToggleState = true;
 
 function handleToggle() {
@@ -28,11 +29,13 @@ function handleToggle() {
 
     <div class="h-screen">
         <NavBar @toggle-side-bar="handleToggle" />
-        <div class="h-5/6">
+        <div class="h-5/6 relative">
             <!-- side bar 1 -->
-            <div class="absolute w-full h-full bg-transparent flex drop-shadow-xl">
+            <div
+                class="absolute w-full h-full bg-transparent flex drop-shadow-xl"
+            >
                 <div
-                    class="w-0 bg-white  h-auto  overflow-y-auto transition-all duration-300 border-r"
+                    class="w-0 bg-white h-auto overflow-y-auto transition-all duration-300 border-r"
                     :class="{
                         '!w-1/4': toggleState === 1 || toggleState === 2,
                     }"
@@ -42,7 +45,7 @@ function handleToggle() {
                 </div>
                 <!-- side bar 2 -->
                 <div
-                    class="w-0 bg-white  overflow-y-auto  transition-all duration-300 border-r space-y-3"
+                    class="w-0 bg-white overflow-y-auto transition-all duration-300 border-r space-y-3"
                     :class="{
                         'w-2/4 px-6': toggleState === 2,
                     }"
@@ -56,9 +59,90 @@ function handleToggle() {
             </div>
 
             <main>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem eligendi eos, voluptate tempora molestiae iusto, dolorum deserunt voluptatibus nulla, aliquid tenetur corrupti architecto. Quis eum eaque, quam tenetur non nostrum.</p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Rem eligendi eos, voluptate tempora molestiae iusto, dolorum
+                    deserunt voluptatibus nulla, aliquid tenetur corrupti
+                    architecto. Quis eum eaque, quam tenetur non nostrum.
+                </p>
             </main>
+
+            <div
+                class="absolute right-0 top-0 border w-full h-full bg-transparent"
+            >
+                <div
+                    class="relative border w-0 h-full ml-auto bg-white transition-all space-y-8 duration-300"
+                    :class="{ 'w-1/2 p-4': showStatBar }"
+                >
+                <!-- stat bar toggle -->
+                    <div 
+                        class="absolute h-8 w-8 right-full top-2 bg-white flex justify-center items-center text-xl"
+                        @click="showStatBar = !showStatBar"
+                    >
+                        <div class="simple-icon-star"></div>
+                    </div>
+
+                    <div class="space-y-4">
+                        <p class="text-xs font-light text-gray-400">Status</p>
+
+                        <div class="space-y-2 text-sm">
+                            <div class="flex justify-between">
+                                <div class="simple-icon-star"></div>
+                                <p>Active Surveys</p>
+                                <div>12</div>
+                            </div>
+                            <div class="flex justify-between">
+                                <div class="simple-icon-star"></div>
+                                <p>Active Surveys</p>
+                                <div>12</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="space-y-4">
+                        <p class="text-xs font-light text-gray-400">Categories</p>
+                        
+                        <div class="space-y-2 text-sm">
+                            <div class="flex ">
+                                <input type="checkbox" name="" id="" class="rounded mr-4" />
+                                <p>Active Surveys</p>
+                            </div>
+                            <div class="flex ">
+                                <input type="checkbox" name="" id="" class="rounded mr-4" />
+                                <p>Active Surveys</p>
+                            </div>
+                            <div class="flex ">
+                                <input type="checkbox" name="" id="" class="rounded mr-4" />
+                                <p>Active Surveys</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+
+                    <div class="space-y-4">
+                        <p class="text-xs font-light text-gray-400">Labels</p>
+
+                        <div class="space-y-2 text-sm">
+                            <div>
+                                <div class="border border-blue-500 rounded-lg w-fit px-2">
+                                    New Framework
+                                </div>
+                            </div>
+                            <div>
+                                <div class="border border-blue-500 rounded-lg w-fit px-2">
+                                    Education
+                                </div>
+                            </div>
+                            <div>
+                                <div class="border border-blue-500 rounded-lg w-fit px-2">
+                                    Personal
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        
     </div>
 </template>
